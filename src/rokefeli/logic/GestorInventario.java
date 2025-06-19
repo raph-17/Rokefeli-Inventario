5731b7bd361c2d25643dac89f2520592e0acda18
raph-17 <ricardoplazaxdd@gmail.com>
2025-06-18 23:37:43 -0500

    estoy agregando la funcionalidad de agregar materia prima y de mostrarla en el jframe

--- src/rokefeli/logic/GestorInventario.java
+++ src/rokefeli/logic/GestorInventario.java
@@ -1,8 +1,27 @@
 package rokefeli.logic;
 
+import java.time.LocalDate;
 import rokefeli.model.*;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class GestorInventario {
+    private LinkedList<Insumo> inventarioInsumos = new LinkedList();
+    public LinkedList<LoteMielCosecha> inventarioLotes = new LinkedList();
+    private LinkedList<ProductoFinal> inventarioProductos = new LinkedList();
+    
+    static LoteMielCosecha nuevoLote = new LoteMielCosecha("id01", "eucalipto", "sierra", LocalDate.now(), 300);
+    
+    public void mostrarLotes() {
+       for (LoteMielCosecha lote: inventarioLotes) {
+           System.out.println(lote.getIdLote()+" - "+lote.getCantKg()+" - "+lote.getEstado()+" - "+lote.getFloracion()+" - "+lote.getOrigen()+" - "+lote.getFechaCompra());
+       }
+    }
+    
+    public void main(String[] args) {
+        inventarioLotes.add(nuevoLote);
+        mostrarLotes();
+    }
+    
     
 }
