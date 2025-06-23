2d90a714547a6b6ec5329242721f176b8ba4e0ae
Daniel <Daniel@super152>
2025-06-23 11:12:24 -0500

    Ricardo, agregué el get de idLote para el método transformar, no es lo óptimo, si pudieses revisarlo por favor.

--- src/rokefeli/model/ProductoFinal.java
+++ src/rokefeli/model/ProductoFinal.java
@@ -5,11 +5,13 @@ public class ProductoFinal {
     private String descripcion;    
     private String loteAsociado;
     private int stock;
+    private String idLote;
 
     public ProductoFinal(String sku, String descripcion, String loteAsociado, String idLote, int stock) {
-        this.sku = sku;
+     
         this.descripcion = descripcion;
         this.loteAsociado = loteAsociado;
+        this.idLote = idLote;
         this.stock = stock;
     }
 
@@ -17,32 +19,20 @@ public class ProductoFinal {
         return sku;
     }
 
-    public void setSku(String sku) {
-        this.sku = sku;
-    }
-
     public String getDescripcion() {
         return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
     }
 
     public String getLoteAsociado() {
         return loteAsociado;
     }
 
-    public void setLoteAsociado(String loteAsociado) {
-        this.loteAsociado = loteAsociado;
+    public String getIdLote() {
+        return idLote;
     }
-    
+
     public int getStock() {
         return stock;
     }
 
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
-    
 }
